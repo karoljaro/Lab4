16f7ada04909c17e063eb117bce720d1212965d6
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-05-06 23:33:05 +0200

    Add create_logs function to generate log files with script details

--- skrypt.sh
+++ skrypt.sh
@@ -4,10 +4,25 @@ show_date() {
   date
 }
 
+create_logs() {
+  script_name=$(basename "$0")
+  current_date=$(date)
+  for i in $(seq 1 100); do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: ${filename}" > "${filename}"
+    echo "Nazwa skryptu: ${script_name}" >> "${filename}"
+    echo "Data utworzenia: ${current_date}" >> "${filename}"
+    echo "Utworzono plik: ${filename}"
+  done
+}
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --date)
             show_date
+            ;;
+         --logs)
+            create_logs
             ;;
         *)
             echo "Nieznana flaga: $1"
