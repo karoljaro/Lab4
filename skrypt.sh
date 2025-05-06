2562332838c9c5583577fcce65d12328c551da47
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-05-06 23:52:02 +0200

    Add initialize_repo function to clone repository and update PATH

--- skrypt.sh
+++ skrypt.sh
@@ -18,6 +18,19 @@ create_logs() {
   done
 }
 
+initialize_repo() {
+  echo "Klonowanie repozytorium..."
+  git clone https://github.com/karoljaro/Lab4.git
+  if [ $? -eq 0 ]; then
+    echo "Repozytorium sklonowane do katalogu Lab4_cloned_repo."
+    export PATH="$(pwd)/Lab4_cloned_repo:$PATH"
+    echo "Dodano ścieżkę $(pwd)/Lab4_cloned_repo do PATH dla bieżącej sesji."
+    echo "Aby zmiany w PATH były trwałe, dodaj powyższą linię 'export PATH...' do swojego pliku konfiguracyjnego powłoki (np. ~/.bashrc)."
+  else
+    echo "Błąd podczas klonowania repozytorium."
+  fi
+}
+
 show_help() {
   echo "Dostępne flagi:"
   echo "  --date        Wyświetla dzisiejszą datę"
@@ -33,6 +46,9 @@ while [[ "$#" -gt 0 ]]; do
          --logs | -l)
             create_logs "$2"
             shift
+            ;;
+         --init)
+            initialize_repo
             ;;
          --help | -h)
             show_help
